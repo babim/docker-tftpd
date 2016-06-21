96173bd743d21a165e9e0ae151c9911e21e64573
atomney <atomney@gmail.com>
2016-06-21 12:35:05 -0700

    Create tftpd.sh

---
+++ tftpd.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This script is a wrapper for https://hub.docker.com/atomney/tftpd
+
+
+########## VARIABLES
+TFTPROOT=`pwd`
+
+echo "Your files will be saved in:"
+echo $TFTPROOT
+
+TFTPPORT=69
+
+CONTAINERNAME=tftpd
+
+CURRENTUSER=$(id -u `whoami`)
+#echo $CURRENTUSER
+
+########## INSTALL
+if [ "$1" = "--install" ]; then
+
+        docker run -d -v $TFTPROOT:/data -p $TFTPPORT:69/udp --name $CONTAINERNAME atomney/tftpd
+fi
+
+
+########## TEMP
+if [ "$1" = "--temp" ]; then
+
+        docker run -d -p $TFTPPORT:69/udp --name $CONTAINERNAME atomney/tftpd
+fi
+
+
+########## REMOVE 
+if [ "$1" = "--remove" ]; then
+
+        bash $0 --stop
+        docker rm $CONTAINERNAME
+fi
+
+########## START 
+if [ "$1" = "--start" ]; then
+
+        docker start $CONTAINERNAME
+fi
+
+########## STOP 
+if [ "$1" = "--stop" ]; then
+
+        docker stop $CONTAINERNAME
+fi
